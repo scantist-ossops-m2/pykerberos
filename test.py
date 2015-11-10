fbfc8254b4775a05a6d046cea04fdbdcf66c4ce9
Stefan Richter <stefan@02strich.de>
2015-11-09 19:59:44 -0800

    Make test.py py3 compatible

--- test.py
+++ test.py
@@ -76,49 +76,49 @@ def main():
         if arg in allowedActions:
             actions.add(arg)
         else:
-            print "Action not allowed: %s" % (arg,)
+            print("Action not allowed: %s" % (arg,))
             sys.exit(1)
 
     # Get service principal
     if "service" in actions:
-        print "\n*** Running Service Principal test"
+        print("\n*** Running Service Principal test")
         s, h = service.split("@")
         testServicePrincipal(s, h);
 
     # GSS Basic test
     if "basic" in actions:
         if (len(user) != 0) and (len(pswd) != 0):
-            print "\n*** Running basic test"
+            print("\n*** Running basic test")
             testCheckpassword(user, pswd, service, realm)
         else:
-            print "\n*** Skipping basic test: no user or password specified"
+            print("\n*** Skipping basic test: no user or password specified")
 
     # Full GSSAPI test
     if "gssapi" in actions:
-        print "\n*** Running GSSAPI test"
+        print("\n*** Running GSSAPI test")
         testGSSAPI(service)
 
     if "server" in actions:
-        print "\n*** Running HTTP test"
+        print("\n*** Running HTTP test")
         testHTTP(host, port, use_ssl, service)
 
-    print "\n*** Done\n"
+    print("\n*** Done\n")
 
 def testServicePrincipal(service, hostname):
     try:
         result = kerberos.getServerPrincipalDetails(service, hostname)
-    except kerberos.KrbError, e:
-        print "Kerberos service principal for %s/%s failed: %s" % (service, hostname, e[0])
+    except kerberos.KrbError as e:
+        print("Kerberos service principal for %s/%s failed: %s" % (service, hostname, e[0]))
     else:
-        print "Kerberos service principal for %s/%s succeeded: %s" % (service, hostname, result)
+        print("Kerberos service principal for %s/%s succeeded: %s" % (service, hostname, result))
 
 def testCheckpassword(user, pswd, service, realm):
     try:
         kerberos.checkPassword(user, pswd, service, realm)
-    except kerberos.BasicAuthError, e:
-        print "Kerberos authentication for %s failed: %s" % (user, e[0])
+    except kerberos.BasicAuthError as e:
+        print("Kerberos authentication for %s failed: %s" % (user, e[0]))
     else:
-        print "Kerberos authentication for %s succeeded" % user
+        print("Kerberos authentication for %s succeeded" % user)
 
 def testGSSAPI(service):
     def statusText(r):
@@ -130,39 +130,39 @@ def testGSSAPI(service):
             return "Error"
 
     rc, vc = kerberos.authGSSClientInit(service);
-    print "Status for authGSSClientInit = %s" % statusText(rc);
+    print("Status for authGSSClientInit = %s" % statusText(rc))
     if rc != 1:
         return
 
     rs, vs = kerberos.authGSSServerInit(service);
-    print "Status for authGSSServerInit = %s" % statusText(rs);
+    print("Status for authGSSServerInit = %s" % statusText(rs))
     if rs != 1:
         return
 
     rc = kerberos.authGSSClientStep(vc, "");
-    print "Status for authGSSClientStep = %s" % statusText(rc);
+    print("Status for authGSSClientStep = %s" % statusText(rc))
     if rc != 0:
         return
 
     rs = kerberos.authGSSServerStep(vs, kerberos.authGSSClientResponse(vc));
-    print "Status for authGSSServerStep = %s" % statusText(rs);
+    print("Status for authGSSServerStep = %s" % statusText(rs))
     if rs == -1:
         return
 
     rc = kerberos.authGSSClientStep(vc, kerberos.authGSSServerResponse(vs));
-    print "Status for authGSSClientStep = %s" % statusText(rc);
+    print("Status for authGSSClientStep = %s" % statusText(rc))
     if rc == -1:
         return
 
-    print "Server user name: %s" % kerberos.authGSSServerUserName(vs);
-    print "Server target name: %s" % kerberos.authGSSServerTargetName(vs);
-    print "Client user name: %s" % kerberos.authGSSClientUserName(vc);
+    print("Server user name: %s" % kerberos.authGSSServerUserName(vs))
+    print("Server target name: %s" % kerberos.authGSSServerTargetName(vs))
+    print("Client user name: %s" % kerberos.authGSSClientUserName(vc))
 
     rc = kerberos.authGSSClientClean(vc);
-    print "Status for authGSSClientClean = %s" % statusText(rc);
+    print("Status for authGSSClientClean = %s" % statusText(rc))
 
     rs = kerberos.authGSSServerClean(vs);
-    print "Status for authGSSServerClean = %s" % statusText(rs);
+    print("Status for authGSSServerClean = %s" % statusText(rs))
 
 def testHTTP(host, port, use_ssl, service):
 
@@ -194,16 +194,16 @@ def testHTTP(host, port, use_ssl, service):
     response = sendRequest(host, port, use_ssl, "OPTIONS", uri, {})
 
     if response is None:
-        print "Initial HTTP request to server failed"
+        print("Initial HTTP request to server failed")
         return
 
     if response.status != 401:
-        print "Initial HTTP request did not result in a 401 response"
+        print("Initial HTTP request did not result in a 401 response")
         return
 
     hdrs = response.msg.getheaders("www-authenticate")
     if (hdrs is None) or (len(hdrs) == 0):
-        print "No www-authenticate header in initial HTTP response."
+        print("No www-authenticate header in initial HTTP response.")
     for hdr in hdrs:
         hdr = hdr.strip()
         splits = hdr.split(' ', 1)
@@ -212,19 +212,19 @@ def testHTTP(host, port, use_ssl, service):
         else:
             break
     else:
-        print "No www-authenticate header with negotiate in initial HTTP response."
+        print("No www-authenticate header with negotiate in initial HTTP response.")
         return
 
     try:
         rc, vc = kerberos.authGSSClientInit(service=service);
-    except kerberos.GSSError, e:
-        print "Could not initialize GSSAPI: %s/%s" % (e[0][0], e[1][0])
+    except kerberos.GSSError as e:
+        print("Could not initialize GSSAPI: %s/%s" % (e[0][0], e[1][0]))
         return
 
     try:
         kerberos.authGSSClientStep(vc, "");
-    except kerberos.GSSError, e:
-        print "Could not do GSSAPI step with continue: %s/%s" % (e[0][0], e[1][0])
+    except kerberos.GSSError as e:
+        print("Could not do GSSAPI step with continue: %s/%s" % (e[0][0], e[1][0]))
         return
 
     hdrs = {}
@@ -234,16 +234,16 @@ def testHTTP(host, port, use_ssl, service):
     response = sendRequest(host, port, use_ssl, "OPTIONS", uri, hdrs)
 
     if response is None:
-        print "Second HTTP request to server failed"
+        print("Second HTTP request to server failed")
         return
 
     if response.status/100 != 2:
-        print "Second HTTP request did not result in a 2xx response: %d" % (response.status,)
+        print("Second HTTP request did not result in a 2xx response: %d" % (response.status,))
         return
 
     hdrs = response.msg.getheaders("www-authenticate")
     if (hdrs is None) or (len(hdrs) == 0):
-        print "No www-authenticate header in second HTTP response."
+        print("No www-authenticate header in second HTTP response.")
         return
     for hdr in hdrs:
         hdr = hdr.strip()
@@ -253,22 +253,22 @@ def testHTTP(host, port, use_ssl, service):
         else:
             break
     else:
-        print "No www-authenticate header with negotiate in second HTTP response."
+        print("No www-authenticate header with negotiate in second HTTP response.")
         return
 
     try:
         kerberos.authGSSClientStep(vc, splits[1])
-    except kerberos.GSSError, e:
-        print "Could not verify server www-authenticate header in second HTTP response: %s/%s" % (e[0][0], e[1][0])
+    except kerberos.GSSError as e:
+        print("Could not verify server www-authenticate header in second HTTP response: %s/%s" % (e[0][0], e[1][0]))
         return
 
     try:
         rc = kerberos.authGSSClientClean(vc);
-    except kerberos.GSSError, e:
-        print "Could not clean-up GSSAPI: %s/%s" % (e[0][0], e[1][0])
+    except kerberos.GSSError as e:
+        print("Could not clean-up GSSAPI: %s/%s" % (e[0][0], e[1][0]))
         return
 
-    print "Authenticated successfully"
+    print("Authenticated successfully")
     return
 
 if __name__=='__main__':
