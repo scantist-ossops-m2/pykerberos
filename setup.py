0b6a7a09fcd5e1b916136d948d6e22da8654a964
Matt Davis <nitzmahone@users.noreply.github.com>
2017-12-07 09:02:20 -0800

    bump repo version to 1.2.1dev

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ if krb5_ver:
 
 setup (
     name = "pykerberos",
-    version = "1.2.0",
+    version = "1.2.1dev",
     description = "High-level interface to Kerberos",
     long_description=long_description,
     license="ASL 2.0",
